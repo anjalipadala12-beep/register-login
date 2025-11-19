7ae3b326bee99b9170a7219e6cbe0971d0d89c9a
anjalipadala12-beep <anjalipadala12@gmail.com>
2025-11-19 12:48:42 +0530

    Add files via upload

---
+++ register.py
@@ -0,0 +1,19 @@
+import streamlit as st
+
+st.title(" Registration Form")
+
+st.form("registration_form")
+name = st.text_input("Full Name")
+email = st.text_input("Email")
+password = st.text_input("Password", type="password")
+gender = st.radio("Gender", ["Male", "Female", "Other"],index=None)
+course = st.multiselect(
+    "Select your course:",
+    ["Python", "SQL", "Web Development", "Data Science","C","Java","c++","Data Analyst"]
+)
+agree = st.checkbox("I agree to the Terms & Conditions")
+
+submit = st.button("Register")
+
+if submit:
+    st.success(f"Registration successful! Welcome, {name}.")
